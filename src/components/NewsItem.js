cc48ec7fa76dd44d02e5c91b506ed2acdce406c7
Mahera Furniturewala <82437476+MaheraFurniturewala@users.noreply.github.com>
2021-09-30 19:57:06 +0530

    add samples and components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'  //rce
 
 export class NewsItem extends Component {
     render() {
+        //destructuring
+        let {title, description} = this.props;
         return (
-            <div>
-                News Item Component
+            <div> 
+                <div className="card" style={{width: '18rem'}}>
+                    <img src="..." className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
         )
     }
